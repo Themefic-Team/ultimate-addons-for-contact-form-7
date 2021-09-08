29af866a5b2181e08f6992df0bebe279a2a2a47f
abuhena_bit <abuhena.office@gmail.com>
2021-09-08 18:27:22 +0600

    Initial Plugin

---
+++ addons/range-slider/js/range-slider.js
@@ -0,0 +1,37 @@
+; (function ($) {
+  'use strict';
+  $(document).ready(function () {
+
+    var handle = $(".slider-handle").data("handle");
+    var min = $(".slider-handle").data("min");
+    var max = $(".slider-handle").data("max");
+    var def = $(".slider-handle").data("default");
+
+    if (handle == 1) {
+
+      var slider = document.getElementById("range");
+      var output = document.getElementById("value");
+      output.innerHTML = slider.value; // Display the default slider value
+
+      // Update the current slider value (each time you drag the slider handle)
+      slider.oninput = function () {
+        output.innerHTML = this.value;
+      }
+
+    } else if (handle == 2) {
+      $("#slider-range").slider({
+        range: true,
+        min: min,
+        max: max,
+        values: [min, def],
+        slide: function (event, ui) {
+          $("#amount").val(ui.values[0] + " - " + ui.values[1]);
+          $(".amount").html(ui.values[0] + " - " + ui.values[1]);
+        }
+      });
+      $("#amount").val($("#slider-range").slider("values", 0) + " - " + $("#slider-range").slider("values", 1));
+      $(".amount").val($("#slider-range").slider("values", 0) + " - " + $("#slider-range").slider("values", 1));
+
+    }
+  })
+})(jQuery);
