325ed800738f2576f8e951822648ac72432abc46
Masum Billah <office.hellomasum@gmail.com>
2023-10-25 18:26:41 +0600

    Isseu fixed for total price

--- addons/multistep/assets/js/progressbar.js
+++ addons/multistep/assets/js/progressbar.js
@@ -8,14 +8,43 @@
                 allNextBtn    = $(this).find('.uacf7-next[data-form-id="' + form_id + '"]'),
                 allPrevBtn    = $(this).find('.uacf7-prev[data-form-id="' + form_id + '"]'),
                 allStepTitle  =  $(this).find('.step-title[data-form-id="' + form_id + '"]');  
+
+
+
+
                 
                 allWells.hide();
 
                 navListItems.click(function (e) {
+                    
                     e.preventDefault();
+
+                    allNextBtn.trigger('click')
+
+                    var curStep = $(this).parent().parent().parent().siblings('.uacf7-multisetp-form').find(`.uacf7-form-${form_id}`).find('uacf7-step ').attr('id'),
+                    curStepBtn = curStep.attr("id"),
+                    nextStepWizard = $('div.setup-panel div a[href="#' + curStepBtn + '"]').parent().next().children("a"),
+                    curInputs = curStep.find("input[type='text'],input[type='url']"),
+                    isValid = true;
+
+                    console.log(curInputs)
+
+                    $(".form-group").removeClass("has-error");
+                    for (var i = 0; i < curInputs.length; i++) {
+                        if (!curInputs[i].validity.valid) {
+                            isValid = false;
+                            $(curInputs[i]).closest(".form-group").addClass("has-error");
+                        }
+                    }
+
+
+                 
+
+
                     var $target = $($(this).attr('href')),
                         title   = $($(this).attr('title-id')),
                         $item   = $(this);
+                       
 
                     if (!$item.hasClass('disabled')) {
                         navListItems.removeClass('uacf7-btn-active').addClass('uacf7-btn-default');
@@ -25,6 +54,7 @@
                         allStepTitle.hide();
                         title.show();
                         $target.find('input:eq(0)').focus();
+                        isValid = true;
                     }
                 });
 
@@ -43,6 +73,9 @@
                         curInputs = curStep.find("input[type='text'],input[type='url']"),
                         isValid = true;
 
+                       
+                    console.log(curStep)  
+
                     $(".form-group").removeClass("has-error");
                     for (var i = 0; i < curInputs.length; i++) {
                         if (!curInputs[i].validity.valid) {
@@ -51,9 +84,11 @@
                         }
                     }
 
-                    //if (isValid)
-                        //nextStepWizard.removeAttr('disabled').trigger('click');
+                    // if (isValid)
+                    //     nextStepWizard.removeAttr('disabled').trigger('click');
+
                 });
+
 
                 //$('div.setup-panel div a.uacf7-btn-active').trigger('click');
                 $('#'+form_id+'step-1.uacf7-step.step-content.step-start').show();
