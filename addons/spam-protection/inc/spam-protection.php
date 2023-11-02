adf05d7af24ae045bdd619420bc42ea244773122
Masum Billah <office.hellomasum@gmail.com>
2023-11-02 17:13:41 +0600

    tag shortcode added

--- addons/spam-protection/inc/spam-protection.php
+++ addons/spam-protection/inc/spam-protection.php
@@ -8,6 +8,9 @@
     class SPAM_PROTECTION{
 
         public function __construct(){
+
+            add_action( 'wpcf7_init', array( $this, 'uacf7_spam_protection_add_shortcodes' ) );
+            add_action( 'admin_init', array( $this, 'uacf7_spam_protection_tag_generator' ) );
             add_filter( 'uacf7_post_meta_options', array($this, 'uacf7_post_meta_options_spam_protection'), 24, 2 ); 
         }
 
@@ -40,6 +43,130 @@
         
             $value['spam_protection'] = $spam_protection; 
             return $value;
+        }
+
+        public function uacf7_spam_protection_tag_generator(){
+            if (!function_exists('wpcf7_add_tag_generator')) {
+                return;
+            }
+        
+            wpcf7_add_tag_generator('uacf7_spam_protection',
+                __('Spam Protection', 'ultimate-addons-cf7'),
+                'uacf7-tg-pane-spam-protection',
+                array($this, 'tg_pane_spam_protection')
+            );
+        }
+
+        public static function tg_pane_spam_protection($contact_form, $args = ''){
+            $args = wp_parse_args($args, array());
+            $uacf7_field_type = 'uacf7_spam_protection';
+            ?>
+        <div class="control-box">
+        <fieldset>
+                    <table class="form-table">
+                    <tbody>
+                            <div class="uacf7-doc-notice"> 
+                                <?php echo sprintf( 
+                                    __( 'Not sure how to set this? Check our step by step  %1s.', 'ultimate-addons-cf7' ),
+                                    '<a href="https://themefic.com/docs/uacf7/free-addons/spam-protection/" target="_blank">documentation</a>'
+                                ); ?> 
+                            </div>
+                            <tr>
+                                <th scope="row"><label for="<?php echo esc_attr($args['content'] . '-name'); ?>"><?php echo esc_html(__('Name', 'ultimate-addons-cf7')); ?></label></th>
+                                <td><input type="text" name="name" class="tg-name oneline" id="<?php echo esc_attr($args['content'] . '-name'); ?>" /></td>
+                            </tr> 
+                            <tr>
+                                <th scope="row"><label for="tag-generator-panel-text-class"><?php echo esc_html__('Class attribute', 'ultimate-addons-cf7'); ?></label></th>
+                                <td><input type="text" name="class" class="classvalue oneline option" id="tag-generator-panel-text-class"></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </fieldset>
+        </div>
+
+        <div class="insert-box">
+            <input type="text" name="<?php echo esc_attr($uacf7_field_type); ?>" class="tag code" readonly="readonly" onfocus="this.select()" />
+
+            <div class="submitbox">
+                <input type="button" class="button button-primary insert-tag" id="prevent_multiple" value="<?php echo esc_attr(__('Insert Tag', 'ultimate-addons-cf7')); ?>" />
+            </div>
+        </div>
+        <?php
+        }
+
+        public function uacf7_spam_protection_add_shortcodes() {
+            wpcf7_add_form_tag( array( 'uacf7_range_slider', 'uacf7_range_slider*' ),
+                array( $this, 'uacf7_spam_protection_tag_handler_callback' ), array( 'name-attr' => true )
+            );
+        } 
+
+        public function uacf7_spam_protection_tag_handler_callback($tag){
+
+            if (empty($tag->name)) {
+                return '';
+            }
+             
+            // /** Enable / Disable Spam Protection */
+            // $wpcf7 = WPCF7_ContactForm::get_current(); 
+            // $formid = $wpcf7->id();
+            // $submission = uacf7_get_form_option( $formid, 'submission_id' );
+            // $uacf7_submission_id_enable = isset($submission['uacf7_submission_id_enable']) ? $submission['uacf7_submission_id_enable'] : false; 
+            
+            // if($uacf7_submission_id_enable != true){
+            //     return;
+            // }
+        
+            // $validation_error = wpcf7_get_validation_error($tag->name);
+        
+            // $class = wpcf7_form_controls_class($tag->type);
+        
+        
+            // if ($validation_error) {
+            //     $class .= 'wpcf7-not-valid';
+            // }
+        
+            // $atts = array();
+        
+            // $atts['class'] = $tag->get_class_option($class);
+            // $atts['id'] = $tag->get_id_option();
+            // $atts['tabindex'] = $tag->get_option('tabindex', 'signed_int', true);
+        
+            // if ($tag->is_required()) {
+            //     $atts['aria-required'] = 'true';
+            // }
+        
+            // $atts['aria-invalid'] = $validation_error ? 'true' : 'false';
+        
+            // $atts['name'] = $tag->name;
+        
+            // // input size
+            // $size = $tag->get_option('size', 'int', true);
+            // if ($size) {
+            //     $atts['size'] = $size;
+            // } else {
+            //     $atts['size'] = 40;
+            // } 
+            // $value = $tag->values;
+            // $default_value = $tag->get_default_option($value);
+        
+        
+            // $value = isset($submission['uacf7_submission_id']) ? $submission['uacf7_submission_id'] : '';
+        
+            // $atts['value'] = $value;
+        
+            // $atts['name'] = $tag->name;
+        
+            // $atts = wpcf7_format_atts($atts);
+        
+        
+           
+        
+            // $spam_protection_buffer = ob_get_clean();
+
+            // return $spam_protection_buffer;
+       
+            // return apply_filters( 'uacf7_range_slider_style_pro_feature', $spam_protection_buffer, $tag); 
+        
         }
     }
 
