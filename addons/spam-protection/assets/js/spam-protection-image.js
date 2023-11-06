3b809c796d37ada450a3f014c9cad9bf72c4b1f8
Masum Billah <office.hellomasum@gmail.com>
2023-11-06 15:14:28 +0600

    multiple validation done

--- addons/spam-protection/assets/js/spam-protection-image.js
+++ addons/spam-protection/assets/js/spam-protection-image.js
@@ -7,6 +7,7 @@
         const refreshButton = form_div.find("#refresh");
         const captcha   = form_div.find("#captcha");
         const validate   = form_div.find("#validate");
+        let predefined_flag = true;
         
 
         const captchaCodes = [];
@@ -34,21 +35,37 @@
         }
 
         function validateCaptcha() {
-            const userInput = form_div.find("#userInput").val();
-            const captcha   = form_div.find("#captcha").text();
-            const resultDiv = form_div.find("#result");
-            
-            if (userInput === captcha) {
-                resultDiv.text("CAPTCHA validated successfully!");
-            } else {
-                resultDiv.text("CAPTCHA validation failed. Please try again.");
-            }
+                const userInput = form_div.find("#userInput").val();
+                const captcha   = form_div.find("#captcha").text();
+                const resultDiv = form_div.find("#result");
+                
+                if (userInput === captcha) {
+                    resultDiv.text("CAPTCHA validated successfully!");
+                    setTimeout(() => {
+                      
+                      resultDiv.text("");
+                    }, 2000);
+                } else {
+                    if(predefined_flag){
+
+                        resultDiv.text("CAPTCHA validation failed. Please try again.");
+                        setTimeout(() => {
+                        
+                          resultDiv.text("");
+                        }, 2000);
+        
+                          $(`.uacf7-form-${formId} input[type="submit"]`).on('click ', function (e) {e.preventDefault()});
+        
+                      } 
+                }
             }
 
         
             refreshButton.click(function (e) {
                 e.preventDefault();
                 generateCaptcha();
+                predefined_flag = !predefined_flag;
+                form_div.find("#userInput").val('');
           
             });
 
