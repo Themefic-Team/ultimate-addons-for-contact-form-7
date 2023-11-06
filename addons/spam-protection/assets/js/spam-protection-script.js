fe71e3de2d6db64b539a004c3fc6f5bce918d2ab
Masum Billah <office.hellomasum@gmail.com>
2023-11-06 11:58:18 +0600

    generating random numbers on refresh

--- addons/spam-protection/assets/js/spam-protection-script.js
+++ addons/spam-protection/assets/js/spam-protection-script.js
@@ -5,14 +5,14 @@
         var formId                = $(this).find('input[name="_wpcf7"]').val();
         var uacf7_spam_protection = $('.uacf7-form-'+formId).find('.uacf7_spam_recognation');
 
-        const refreshButton = uacf7_spam_protection.find("#arithmathic_refresh");
-        const validate   = uacf7_spam_protection.find("#arithmathic_validate");
+        var refreshButton = uacf7_spam_protection.find("#arithmathic_refresh");
+        var validate   = uacf7_spam_protection.find("#arithmathic_validate");
 
-        var first_random_number   = Math.random() * 10;
-        var second_random_number  = Math.random() * 10;
-
+    
 
         function uacf7_generate_ramdom_numbers(){
+          var first_random_number   = Math.random() * 10;
+          var second_random_number  = Math.random() * 10;
           uacf7_spam_protection.find('#arithmathic_recognation').find('#frn').text(Math.ceil(first_random_number));
           uacf7_spam_protection.find('#arithmathic_recognation').find('#srn').text(Math.ceil(second_random_number));
         }
@@ -27,14 +27,14 @@
 
 
         function validateCaptcha() {
-          const userInput = uacf7_spam_protection.find("#rtn").val();
-          const resultDiv = uacf7_spam_protection.find("#arithmathic_result");
-          
-          if (userInput == total_number) {
-              resultDiv.text("CAPTCHA validated successfully!");
-          } else {
-              resultDiv.text("CAPTCHA validation failed. Please try again.");
-          }
+            const userInput = uacf7_spam_protection.find("#rtn").val();
+            const resultDiv = uacf7_spam_protection.find("#arithmathic_result");
+            
+            if (userInput == total_number) {
+                resultDiv.text("CAPTCHA validated successfully!");
+            } else {
+                resultDiv.text("CAPTCHA validation failed. Please try again.");
+            }
           }
 
       
@@ -43,6 +43,8 @@
               uacf7_generate_ramdom_numbers();
         
           });
+
+      
 
           validate.click(function (e) {
               e.preventDefault();
