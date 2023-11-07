c7bed174f1ed68f9b2ca057053352d3e1a4486fe
Masum Billah <office.hellomasum@gmail.com>
2023-11-07 10:40:02 +0600

    spam-protection-arithmetic file name changed

---
+++ addons/spam-protection/assets/js/spam-protection-arithmetic.js
@@ -0,0 +1,125 @@
+(function($){
+
+    var forms = $('.wpcf7-form'); 
+    forms.each(function(){
+        var formId                = $(this).find('input[name="_wpcf7"]').val();
+        var uacf7_spam_protection = $('.uacf7-form-'+formId).find('.uacf7_spam_recognation');
+
+        var refreshButton = uacf7_spam_protection.find("#arithmathic_refresh");
+        var validate      = uacf7_spam_protection.find("#arithmathic_validate");
+        var is_applied    = uacf7_spam_protection.attr("is_applied");
+
+        let predefined_flag       = true;
+        let main_flag             = true;
+
+    
+
+
+        // Generating Random Numbers
+        function uacf7_generate_ramdom_numbers(){
+          var first_random_number   = Math.random() * 10;
+          var second_random_number  = Math.random() * 10;
+          uacf7_spam_protection.find('#arithmathic_recognation').find('#frn').text(Math.ceil(first_random_number));
+          uacf7_spam_protection.find('#arithmathic_recognation').find('#srn').text(Math.ceil(second_random_number));
+        }
+        uacf7_generate_ramdom_numbers();
+        
+   
+
+        //Returning Total Sum of Numbers
+        function return_total_num (){
+          var first_number      = uacf7_spam_protection.find('#arithmathic_recognation').find('#frn').text();
+          var first_number_int  = parseInt(first_number);
+          var second_number     = uacf7_spam_protection.find('#arithmathic_recognation').find('#srn').text();
+          var second_number_int = parseInt(second_number);
+          
+          var total_number = first_number_int + second_number_int;
+
+          return total_number;
+        }
+
+    
+
+        //Checking wether the user given data is true
+        function validateCaptcha() {
+            const userInput = uacf7_spam_protection.find("#rtn").val();
+            const resultDiv = uacf7_spam_protection.find("#arithmathic_result");
+            
+            if (userInput == return_total_num()) {
+              main_flag = false;
+
+              resultDiv.text("CAPTCHA validated successfully!");
+              setTimeout(() => {
+                
+                resultDiv.text("");
+              }, 2000);
+           
+             
+            } else {
+
+              if(predefined_flag){
+
+                resultDiv.text("CAPTCHA validation failed. Please try again.");
+                setTimeout(() => {
+                
+                  resultDiv.text("");
+                }, 2000);
+
+                  if(is_applied === 'applied'){
+
+                    $(`.uacf7-form-${formId} input[type="submit"]`).on('click ', function (e) {e.preventDefault()});
+
+                  }
+
+
+
+              }  
+            }
+          }
+
+      
+          //Refresh button action
+          refreshButton.click(function (e) {
+              e.preventDefault();
+              main_flag = false;
+              uacf7_spam_protection.find("#rtn").val('');
+              uacf7_generate_ramdom_numbers();
+              predefined_flag = !predefined_flag;
+        
+          });
+
+      
+          //Validate Button action
+          validate.click(function (e) {
+              e.preventDefault();
+              validateCaptcha();
+          });
+
+
+          //Conditionally make submission event false
+          if(is_applied === 'applied'){
+            $(document).ready(function (){
+              if(main_flag === true){
+                  $(`.uacf7-form-${formId} input[type="submit"]`).on('click ', function (e) {
+                    e.preventDefault();
+                    const resultDiv = form_div.find("#result");
+                          resultDiv.text('Validate Captcha First !');
+                          setTimeout(() => {
+                              resultDiv.text('');
+                              
+                          }, 2000);
+                  });
+              }
+            });
+          }
+
+         
+
+         
+        
+        
+  }); 
+
+})(jQuery);
+
+
