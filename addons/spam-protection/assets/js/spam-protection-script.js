e7e1e8c39e3c426e69f2f329a9ec80452b8b9836
Masum Billah <office.hellomasum@gmail.com>
2023-11-09 11:45:13 +0600

    a little bit changes to main script

--- addons/spam-protection/assets/js/spam-protection-script.js
+++ addons/spam-protection/assets/js/spam-protection-script.js
@@ -13,6 +13,8 @@
 
        
         $(document).ready(function() {
+
+            //Time based submission Controls
             $(uacf7_form).submit(function(event) {
      
                 var formSubmitTime = new Date().getTime();
@@ -23,53 +25,26 @@
                     return false;
                 }
         
-                                                                                                                                 
-                // If no issues found, allow form submission
                 return true;
             });
-        });
+    
 
-
-        // Detect Wrong Mail Address
-
-        // $(uacf7_mail).on('change', function (){
-
-        //     $(document).ready(function() {
-        //         var current_mail = $(this).val();
-        //         function isValidEmail(email) {
-        //             return validator.isEmail(email);
-        //         }
-
-        //         const email = "test@exampl755e.com";
-        //         if (isValidEmail(email)) {
-        //             console.log("Email is valid");
-        //         } else {
-        //             console.log("Email is invalid");
-        //         }
-
-            
-        //     });
-
-        // });
-
-
-            // Ban Enlisted IPs
-
-            $(document).ready(function() {
-
-                const bannedIPs = ['127.0.0.1', '10.0.0.2', '127.0.0.1'];
-            
-                $(uacf7_form).on('click', function(event) {
- 
-
-                    if ($.inArray(user_ip, bannedIPs) !== -1) {
-                        alert('Your IP address is banned from submitting this form.');
-                        // event.preventDefault(); 
-                    }         
-                    
-                });
+            //IP Ban
+            const bannedIPs = ['127.0.0.1', '10.0.0.2', '127.0.0.1'];
         
+            $(uacf7_form).on('click', function(event) {
+
+
+                if ($.inArray(user_ip, bannedIPs) !== -1) {
+                    alert('Your IP address is banned from submitting this form.');
+                    // event.preventDefault(); 
+                }         
+                
             });
+
+            //Country Ban
+    
+        });
 
 
     });
