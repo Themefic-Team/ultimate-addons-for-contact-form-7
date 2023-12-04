01ac142daaad954bc5badf9e6c9e16fd649f41c7
Masum Billah <office.hellomasum@gmail.com>
2023-12-04 18:31:02 +0600

    nested date issue arrised

--- addons/material-design/assets/js/uacf7-md-one-script.js
+++ addons/material-design/assets/js/uacf7-md-one-script.js
@@ -17,19 +17,27 @@ $(document).ready(function($){
 
 	/*For Two Column*/
 
-	$('.TF_Field_Wrap').find("input").on('focus input', function() {
-		$(this).parent().siblings('label').addClass('has-value');
-	})
-	.blur(function() {
-		var text_val = $(this).val();
-		if(text_val === "") {
-			$(this).parent().siblings('label').removeClass('has-value');
-		}
-	});
+	
 
+		$('.TF_Field_Wrap').find("input").on('focus', function() {
+			$(this).parent().siblings('label').addClass('has-value');
+		})
+		.blur(function() {
+
+			if ($('input[type="date"]').length === 0) {
+				var text_val = $(this).val();
+				if(text_val === "") {
+					$(this).parent().siblings('label').removeClass('has-value');
+				}
+			}
+		});
+
+	
+	
 	/** For Date and Select */
 
 	$('.TFDate, .wpcf7-select').parent().siblings('label').addClass('has-value');
+	$('.TF_Field_Wrap').find('.TFDate').parent().siblings('label').addClass('has-value');
 
 });
 
