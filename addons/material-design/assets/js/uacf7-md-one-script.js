9ac136143a811e2f94b189db409db3ae44314815
Masum Billah <office.hellomasum@gmail.com>
2023-12-05 15:59:05 +0600

    added script to design two

--- addons/material-design/assets/js/uacf7-md-one-script.js
+++ addons/material-design/assets/js/uacf7-md-one-script.js
@@ -1,48 +1,53 @@
 
 (function($){
 
-$(document).ready(function($){
+	var forms = $('.wpcf7-form');
 
-	var Uacf7selectedElements = $('.uacf7-material-design [class^="uacf7-form-"] .TFText, .TFEmail, .TFTel, .TFUrl, .TFnum');
-	var Uacf7selectedElementDateandSelect = $('.uacf7-material-design [class^="uacf7-form-"] .TFDate, .wpcf7-select, .TFFile');
-	var Uacf7selectedElementsNested = $('.uacf7-material-design [class^="uacf7-form-"] .TF_Field_Wrap');
-	/** For General Fields */
-	$(Uacf7selectedElements).on('focus input', function() {
-			$(this).parent().siblings('label').addClass('has-value');
-	})
-	.blur(function() {
-		var text_val = $(this).val();
-		if(text_val === "") {
-			$(this).parent().siblings('label').removeClass('has-value');
-		}
-	});
+  	forms.each(function(){
+
+        var formId = $(this).find('input[name="_wpcf7"]').val();
 
 
-	/*For Two Column*/
+			$(document).ready(function($){
+			
+				var Uacf7selectedElements = $(`.uacf7-material-design .uacf7-form-${formId} .TFText, .TFEmail, .TFTel, .TFUrl, .TFnum`);
+				var Uacf7selectedElementDateandSelect = $(`.uacf7-material-design .uacf7-form-${formId} .TFDate, .wpcf7-select, .TFFile`);
+				var Uacf7selectedElementsNested = $(`.uacf7-material-design .uacf7-form-${formId} .TF_Field_Wrap`);
+				/** For General Fields */
+				$(Uacf7selectedElements).on('focus input', function() {
+						$(this).parent().siblings('label').addClass('has-value');
+				})
+				.blur(function() {
+					var text_val = $(this).val();
+					if(text_val === "") {
+						$(this).parent().siblings('label').removeClass('has-value');
+					}
+				});
+			
+			
+				/*For Two Column*/
+					$(Uacf7selectedElementsNested).find('input:not([type="date"]').on('focus', function() {
+						$(this).parent().siblings('label').addClass('has-value');
+					})
+					.blur(function() {
+							var text_val = $(this).val();
+					
+							if(text_val === "") {
+								$(this).parent().siblings('label').removeClass('has-value');
+						}
+			
+					});
+			
+				
+				/** For Date and Select */
+			
+				$(Uacf7selectedElementDateandSelect).parent().siblings('label').addClass('has-value');
+				$(Uacf7selectedElementsNested).find('.TFDate').parent().siblings('label').addClass('has-value');
+			
+			});
+			
 
-
-		$(Uacf7selectedElementsNested).find('input:not([type="date"]').on('focus', function() {
-			$(this).parent().siblings('label').addClass('has-value');
-		})
-		.blur(function() {
-				var text_val = $(this).val();
-		
-				if(text_val === "") {
-					$(this).parent().siblings('label').removeClass('has-value');
-			}
-
-		});
-
-	
-	/** For Date and Select */
-
-	$(Uacf7selectedElementDateandSelect).parent().siblings('label').addClass('has-value');
-	$(Uacf7selectedElementsNested).find('.TFDate').parent().siblings('label').addClass('has-value');
-
-});
-
-
-
+  });
 })(jQuery);
 
 
--- addons/material-design/assets/js/uacf7-md-two-script.js
+++ addons/material-design/assets/js/uacf7-md-two-script.js
@@ -3,25 +3,49 @@
 
 	var forms = $('.wpcf7-form');
 
-  forms.each(function(){
+  	forms.each(function(){
 
         var formId = $(this).find('input[name="_wpcf7"]').val();
 
-		$(document).ready(function($){
-			$(`.uacf7-form-${formId}`).find(".TFText, .TFEmail, .TFTel, .TFFirst_Name, .TFLast_Name").on('focus input', function() {
-					$(this).parent().siblings('label').addClass('has-value');
-			})
-			.blur(function() {
-				var text_val = $(this).val();
-				if(text_val === "") {
-					$(this).parent().siblings('label').removeClass('has-value');
-				}
+
+			$(document).ready(function($){
+			
+				var Uacf7selectedElements = $(`.uacf7-material-design .uacf7-form-${formId} .TFText, .TFEmail, .TFTel, .TFUrl, .TFnum`);
+				var Uacf7selectedElementDateandSelect = $(`.uacf7-material-design .uacf7-form-${formId} .TFDate, .wpcf7-select, .TFFile`);
+				var Uacf7selectedElementsNested = $(`.uacf7-material-design .uacf7-form-${formId} .TF_Field_Wrap`);
+				/** For General Fields */
+				$(Uacf7selectedElements).on('focus input', function() {
+						$(this).parent().siblings('label').addClass('has-value');
+				})
+				.blur(function() {
+					var text_val = $(this).val();
+					if(text_val === "") {
+						$(this).parent().siblings('label').removeClass('has-value');
+					}
+				});
+			
+			
+				/*For Two Column*/
+					$(Uacf7selectedElementsNested).find('input:not([type="date"]').on('focus', function() {
+						$(this).parent().siblings('label').addClass('has-value');
+					})
+					.blur(function() {
+							var text_val = $(this).val();
+					
+							if(text_val === "") {
+								$(this).parent().siblings('label').removeClass('has-value');
+						}
+			
+					});
+			
+				
+				/** For Date and Select */
+			
+				$(Uacf7selectedElementDateandSelect).parent().siblings('label').addClass('has-value');
+				$(Uacf7selectedElementsNested).find('.TFDate').parent().siblings('label').addClass('has-value');
+			
 			});
-
-			$('.TFDate, .wpcf7-select').parent().siblings('label').addClass('has-value');
-
-		});
-
+			
 
   });
 })(jQuery);
