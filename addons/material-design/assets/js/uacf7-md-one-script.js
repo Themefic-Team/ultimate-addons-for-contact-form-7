4c0d8d62bac0175526f3a12384e2708f71e10896
Masum Billah <office.hellomasum@gmail.com>
2023-12-06 15:19:26 +0600

    added style for select box

--- addons/material-design/assets/js/uacf7-md-one-script.js
+++ addons/material-design/assets/js/uacf7-md-one-script.js
@@ -10,32 +10,27 @@
 
 			$(document).ready(function($){
 			
-				var uacf7SelectedElements             = $(`.uacf7-material-design .uacf7-form-${formId} .TFText, .TFEmail, .TFTel, .TFUrl, .TFnum`);
+				var uacf7SelectedElements             = $(`.uacf7-material-design .uacf7-form-${formId} .TFText, .TFEmail, .TFTel, .TFUrl, .TFnum, .TFDate, .TFFile `);
 				var uacf7SelectedElementDateandSelect = $(`.uacf7-material-design .uacf7-form-${formId} .TFDate, .wpcf7-select, .TFFile`);
 
 
 				/** For General Fields */
 				$(uacf7SelectedElements).on('focus', function() {
 						$(this).parent().siblings('label').addClass('has-value');
-						$(this).css({
-							outline: 'none',
-							border: '2px solid #6747c1'
-						});
+					
 				}).hover( function () {
 					$(this).parent().siblings('label').addClass('has-value');
-					$(this).css({
-						outline: 'none',
-						border: '2px solid #6747c1'
-	
-					});
+					$(this).addClass('hover');
 				}).mouseleave (function (){
 					var text_val = $(this).val();
 					if(text_val !== "") {
 						$(this).parent().siblings('label').addClass('has-value');
 					}
-					$(this).css({
-						border: ''
-					});
+					$(this).removeClass('hover');
+					if(!$(this).hasClass('TFDate') && !$(this).hasClass('TFFile')){
+						$(this).parent().siblings('label').removeClass('has-value');
+					}
+					
 				})
 				.blur(function() {
 					var text_val = $(this).val();
