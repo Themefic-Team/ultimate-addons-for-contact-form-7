f613ee962597449bb0f51a290b41b1b3ce304e93
Masum Billah <office.hellomasum@gmail.com>
2023-12-11 12:19:25 +0600

    added script for outlined|

--- addons/material-design/assets/js/uacf7-md-outlined-script.js
+++ addons/material-design/assets/js/uacf7-md-outlined-script.js
@@ -12,19 +12,25 @@
 
 			$(document).ready(function($){
 			
-				var uacf7SelectedElements             = $(`.uacf7-material-design-outlined .uacf7-form-${formId} .TFText, .TFEmail, .TFTel, .TFUrl, .TFnum, .TFDate, .TFFile `);
+				var uacf7SelectedElements             = $(`.uacf7-material-design-outlined .uacf7-form-${formId} .TFText, .TFEmail, .TFTel, .TFUrl, .TFnum `);
 				var uacf7SelectedElementDateandSelect = $(`.uacf7-material-design-outlined .uacf7-form-${formId} .wpcf7-date, .wpcf7-select, .wpcf7-file, .TFTextarea`);
 				var uacf7SelectedRequired             = $(`.uacf7-material-design-outlined .uacf7-form-${formId}`).find('.wpcf7-validates-as-required');
 
 
 				/** For General Fields */
 				$(uacf7SelectedElements).on('focus', function() {
+
 					$(this).parent().siblings('label').addClass('has-value');
 					$(this).addClass('input-focus');
 					$(this).parent().siblings('label').removeClass('hover-label');
+
 				}).hover(function () {
-					$(this).parent().siblings('label').addClass('hover-label');
-					$(this).addClass('hover');
+
+					if($(this).is(':not(:focus)')){
+						$(this).parent().siblings('label').addClass('hover-label');
+						$(this).addClass('hover');
+					}
+					
 				}).mouseleave(function () {
 					$(this).parent().siblings('label').removeClass('hover-label');
 					var val = $(this).val();
@@ -36,7 +42,9 @@
 						$(this).parent().siblings('label').removeClass('has-value');
 					}
 				}).keypress(function () {
+
 					$(this).parent().siblings('label').addClass('has-value');
+
 				}).on('input', function () {
 					var val = $(this).val();
 					if (val.trim() !== '') {
@@ -60,14 +68,6 @@
 					}	
 				});
 
-
-		
-				
-				/** For Date and Select */
-			
-				$(uacf7SelectedElementDateandSelect).parent().siblings('label').addClass('has-value');
-
-			
 				
 				// if(uacf7SelectedRequired){
 				// 	$(uacf7SelectedRequired).on('blur', function() {
@@ -77,11 +77,37 @@
 		
 				// 	});
 				// }
+			
+		
 
+
+
+			/** For Date, File, Textarea and Select */
 	
-			
-			});
-			
+				$(uacf7SelectedElementDateandSelect).parent().siblings('label').addClass('dfst');
+
+				$(uacf7SelectedElementDateandSelect).on('mouseenter', function () {
+	
+					$(this).parent().siblings('label').addClass('hover-dfst-label');
+					$(this).addClass('hover-dfst');
+					
+				}).on('mouseleave', function () {
+	
+					$(this).parent().siblings('label').removeClass('hover-dfst-label');
+					$(this).removeClass('hover-dfst');
+					
+				}).on('focus', function () {
+
+					$(this).parent().siblings('label').addClass('focus-dfst-label');
+					$(this).addClass('focus-dfst');
+
+				}).on('blur', function (){
+					
+					$(this).parent().siblings('label').removeClass('focus-dfst-label');
+					$(this).removeClass('focus-dfst');
+				});
+				
+		});
 
   });
 })(jQuery);
