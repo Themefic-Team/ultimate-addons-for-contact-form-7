f824a58c845f14f8a70ef9a4cee874d0245a59a2
Masum Billah <office.hellomasum@gmail.com>
2023-12-26 11:24:25 +0600

    added title description script

--- addons/multistep/multistep.php
+++ addons/multistep/multistep.php
@@ -856,8 +856,18 @@ class UACF7_MULTISTEP {
                                 <a title-id=".step-<?php echo esc_attr($step_id); ?>" data-form-id="<?php echo esc_attr($cfform->id()); ?>"   href="#<?php echo esc_attr($cfform->id()); ?>step-<?php echo esc_attr($step_id); ?>" type="button" class="btn <?php if( $step_id == 1 ) { echo esc_attr('uacf7-btn-active'); }else{ echo esc_attr('uacf7-btn-default'); } ?> btn-circle"><?php 
                                     if(is_array($step_name)) {
 
+                                        // if($uacf7_progressbar_style == 'style-7'){
+                                        //     echo '<p>' . $content . '</p>';
+                                        // }
+
                                         if($uacf7_progressbar_style == 'style-7'){
+                                            echo '<div class="uacf7-ms-skin7-title-desc">';
                                             echo '<p>' . $content . '</p>';
+                                            if (isset($multistep_meta['desc_title_' . $step_name[$step_count]])) {
+                                                echo '<span>' . $multistep_meta['desc_title_' . $step_name[$step_count]] . '</span>';
+                                            }
+
+                                            echo '</div>';
                                         }
 
                                         do_action( 'uacf7_progressbar_image', $step_name[$step_count], $cfform->id() );
