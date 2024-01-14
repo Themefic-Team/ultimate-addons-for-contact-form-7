b5d51131a1eb4ccfb5c6361afe1251a4609d66d0
Masum Billah <office.hellomasum@gmail.com>
2024-01-14 09:10:46 +0600

    Phone number tag script added

--- addons/country-dropdown/assets/js/script.js
+++ addons/country-dropdown/assets/js/script.js
@@ -25,5 +25,97 @@
 }); 
 
 
+
+	/**Phone Number Tag Start */
+
+  $(document).ready(function () {
+
+   
+    function uacf7_phone_number_fn(iso2){
+      $('.uacf7_phone_number input[type="tel"]').each(function() {
+
+      
+        var phoneTagName = $(this).attr('phone-tag-name');
+
+        var telInput = $('#phone_' + phoneTagName);
+        
+        telInput.intlTelInput('destroy');
+
+        telInput.intlTelInput({
+          utilsScript: uacf7_localize_obj.plugin_dir_url+'assets/js/utils.js',
+          initialCountry: iso2 ?? 'us',
+        });
+      
+    
+        var validate = function(input) {
+          if ($.trim(input.val())) {
+            if (!input.intlTelInput("isValidNumber")) {
+              $('#vallidation_message_' + phoneTagName).text(uacf7_localize_obj.phone_number_validation_message);
+            } else {
+              $('#vallidation_message_' + phoneTagName).text('');
+            }
+          }
+        };
+      
+        telInput.on('blur', function() {
+          validate(telInput);
+        });
+      });
+    }
+
+
+    uacf7_phone_number_fn();
+
+
+    // var country_field_value   = $('.uacf7-form-'+formId).find('#uacf7_country_select').find('select');
+  
+    // country_field_value.on('change', function () {
+    //   var iso2 = $(this).find('option:selected').attr('iso2');
+    //    uacf7_phone_number_fn(iso2);
+    //    var country_code = $(document).find('.country-list').find('li');
+
+
+    //   country_code.each(function () {
+    //     if($(this).data('country-code') == iso2){
+
+       
+    //     }
+    
+    //   });
+
+    // });
+
+    // Phone Number
+  
+    // $('.uacf7_phone_number input[type="tel"]').each(function() {
+    // 	var phoneTagName = $(this).attr('phone-tag-name');
+
+    // 	var telInput = $('#phone_' + phoneTagName);
+    
+    // 	telInput.intlTelInput({
+    // 		utilsScript: 'https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/8.4.8/js/utils.js',
+    // 		initialCountry: iso2,
+    // 	});
+    
+    // 	var validate = function(input) {
+    // 		if ($.trim(input.val())) {
+    // 			if (!input.intlTelInput("isValidNumber")) {
+    // 				$('#vallidation_message_' + phoneTagName).text(all_country_script.phone_number_validation_message);
+    // 			} else {
+    // 				$('#vallidation_message_' + phoneTagName).text('');
+    // 			}
+    // 		}
+    // 	};
+    
+    // 	telInput.on('blur', function() {
+    // 		validate(telInput);
+    // 	});
+    // });
+    
+  /**Phone Number Tag End */
+
+});
+
+
 })(jQuery);
 
