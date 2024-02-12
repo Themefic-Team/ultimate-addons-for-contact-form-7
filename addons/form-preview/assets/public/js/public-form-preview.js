038ec03e57aabb058ded6bfcd49e224d088fe0dc
Masum Billah <office.hellomasum@gmail.com>
2024-02-12 17:16:32 +0600

    added preview script partially

--- addons/form-preview/assets/public/js/public-form-preview.js
+++ addons/form-preview/assets/public/js/public-form-preview.js
@@ -21,7 +21,32 @@
             //     }
             // });
 
-            alert('hi')
+            $(document).ready(function () {
+                $('#preview-btn').click(function () {
+                    var formData = {};
+                    $('.wpcf7-form input, .wpcf7-form textarea, .wpcf7-form select').each(function () {
+                        var fieldName = $(this).attr('name');
+                        var fieldValue = $(this).val();
+                        formData[fieldName] = fieldValue;
+                    });
+                    showPreviewPopup(formData);
+                });
+            });
+            
+            function showPreviewPopup(formData) {
+                var previewHtml = '<div class="preview-popup">';
+                for (var field in formData) {
+                    if (formData.hasOwnProperty(field)) {
+                        previewHtml += '<p><strong>' + field + ':</strong> ' + formData[field] + '</p>';
+                    }
+                }
+                previewHtml += '</div>';
+                
+                $('.preview-popup').remove(); 
+                $('body').append(previewHtml);
+                $('.preview-popup').fadeIn();
+            }
+            
         
         });
         
