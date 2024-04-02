5e62b7aedd2dc390c113fc1c7353a22f10efcecd
Masum Billah <office.hellomasum@gmail.com>
2024-04-03 04:06:48 +0600

    modified in column addon

--- addons/column/column.php
+++ addons/column/column.php
@@ -217,7 +217,7 @@ class UACF7_COLUMN {
 					
                     $html = '<div class="'.esc_attr($ucaf7_column_class).'">';
 					
-					echo wp_kses_post( apply_filters( 'uacf7_column_custom_width', $html, $ucaf7_column_class, $uacf7_column_custom_width ) );
+					echo _post( apply_filters( 'uacf7_column_custom_width', $html, $ucaf7_column_class, $uacf7_column_custom_width ) );
 					
                 } else if ($form_part == '[/uacf7-col]') {
                     echo '</div>';
