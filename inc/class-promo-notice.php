7a74220ed6ff9a880162232da6a1ffcadc224595
Ashik384 <39790492+Ashik384@users.noreply.github.com>
2024-11-14 12:22:48 +0600

    = 3.4.9 - 14/11/2024 =
    = 3.4.9 - 14/11/2024 =
    
    - Minor bug fixes & improvements
    - System stability improved

--- inc/class-promo-notice.php
+++ inc/class-promo-notice.php
@@ -181,7 +181,7 @@ class UACF7_PROMO_NOTICE {
                 <a href="<?php echo esc_attr( $deal_link ); ?>" style="display: block; line-height: 0;" target="_blank" >
                     <img  style="width: 100%;" src="<?php echo esc_attr($image_url) ?>" alt="">
                 </a> 
-                <?php if( isset($this->uacf7_promo_option['dasboard_dismiss']) && $this->uacf7_promo_option['dasboard_dismiss'] == true): ?>
+                <?php if( isset($dashboard_banner['dismiss_status']) && $dashboard_banner['dismiss_status'] == true): ?>
                 <button type="button" class="notice-dismiss tf_black_friday_notice_dismiss"><span class="screen-reader-text"><?php echo __('Dismiss this notice.', 'ultimate-addons-cf7' ) ?></span></button>
                 <?php  endif; ?>
             </div>
