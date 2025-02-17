c21353cafbfe0f68c337b78bf559d63da35b806a
extenddeveloper <extenddeveloper@gmail.com>
2025-02-17 11:02:01 +0600

    Updated Booking pro sunset notice style

--- inc/functions.php
+++ inc/functions.php
@@ -1314,8 +1314,8 @@ function uacf7_booking_pro_admin_notice() {
     }
 
     ?>
-    <div class="notice notice-danger is-dismissible uacf7-booking-pro-notice">
-        <p><strong>Important Announcement:</strong> We would like to inform you that the <strong>Booking/Appointment Form Pro</strong> add-on will be discontinued in the upcoming update. As part of our ongoing commitment to enhancing user experience, we are introducing <strong>Hydra Booking</strong> Free as its replacement.</p>
+    <div class="notice notice-warning notice-danger is-dismissible uacf7-booking-pro-notice" style="border-left-color: #b32d2e;">
+        <p><strong style="display: block; padding-bottom: 4px; font-size: 16px;">Important Announcement:</strong> We would like to inform you that the <strong>Booking/Appointment Form Pro</strong> add-on will be discontinued in the upcoming update. As part of our ongoing commitment to enhancing user experience, we are introducing <strong>Hydra Booking</strong> Free as its replacement.</p>
     </div>
 
     <script type="text/javascript">
