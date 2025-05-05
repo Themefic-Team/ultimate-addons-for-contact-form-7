92d1480d3e3c68803a16b2a8a88a4fe05bb0e53a
extenddeveloper <extenddeveloper@gmail.com>
2025-05-05 16:30:23 +0600

    Updated drag and drop addon preview and document url

--- admin/tf-options/options/uacf7-settings.php
+++ admin/tf-options/options/uacf7-settings.php
@@ -187,8 +187,8 @@ UACF7_Settings::option(
 								'image_url' => UACF7_URL . 'assets/admin/images/addons/Booking-or-Appointment-Form@2x.png',
 								'default' => false,
 								'subtitle' => __( 'This simple plugin create Drag & Drop or choose Multiple File upload in your Confact Form 7 Forms.', 'ultimate-addons-cf7' ),
-								'demo_link' => 'https://cf7addons.com/preview/contact-form-7-booking-form/',
-								'documentation_link' => 'https://themefic.com/docs/uacf7/pro-addons/contact-form-7-booking-form/',
+								'demo_link' => 'https://cf7addons.com/preview/drag-and-drop-multiple-file-upload/',
+								'documentation_link' => 'https://themefic.com/docs/uacf7/pro-addons/drag-and-drop-file/',
 								'is_pro' => true,
 							),
 							'uacf7_enable_post_submission' => array(
