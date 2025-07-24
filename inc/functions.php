4f60d3fae6f3274231f0f32b4c1747cd8eb88387
extenddeveloper <extenddeveloper@gmail.com>
2025-07-24 13:54:41 +0600

    Added UTM Source to urls

--- inc/functions.php
+++ inc/functions.php
@@ -1977,6 +1977,17 @@ function migrate_redirection_data_to_uacf7() {
 
 }
 
+function uacf7_utm_generator( $url, $utm_params = array() ) {
+	$host_url = parse_url( get_site_url(), PHP_URL_HOST );
+	$utm_params = array_merge( array(
+		'utm_source'   => 'uacf7_' . $host_url,
+		'utm_medium'   => 'plugin',
+		'utm_campaign' => 'uacf7_plugin_installation',
+	), $utm_params );
+
+	$query_string = http_build_query( $utm_params );
+	return esc_url( $url . ( strpos( $url, '?' ) === false ? '?' : '&' ) . $query_string );
+}
 
 
 // function uacf7_check_and_install_hydra_booking($upgrader_object, $options) {
