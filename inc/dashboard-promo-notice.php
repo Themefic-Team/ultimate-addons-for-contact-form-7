965a06df4cb07d26838f4b66dd1e7f965eb7d9b8
extenddeveloper <extenddeveloper@gmail.com>
2026-06-03 16:21:30 +0600

    Updated promotional banner design and style

--- inc/dashboard-promo-notice.php
+++ inc/dashboard-promo-notice.php
@@ -137,10 +137,15 @@ class Uacf7_Dashboard_Promo_Notice {
 					target="_blank"
 					class="button button-primary"
 				>
-					<?php esc_html_e(
-						'Buy Now',
-						'ultimate-addons-cf7'
-					); ?>
+					
+					<div class="buy-now-text">
+						<?php esc_html_e( 'Buy Now', 'ultimate-addons-cf7' ); ?>
+					</div>
+					<div class="arrow-icon">
+						<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+							<path d="M17 17V7H7M17 7L7 17" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+						</svg>
+					</div>
 				</a>
 
 			</div>
