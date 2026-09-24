b4844a622389ad330edfb117a3197f0676da5728
extenddeveloper <extenddeveloper@gmail.com>
2026-09-24 12:00:03 +0600

    Fixed Text editor html scaping issue on save

--- admin/tf-options/classes/UACF7_Metabox.php
+++ admin/tf-options/classes/UACF7_Metabox.php
@@ -227,7 +227,7 @@ if ( ! class_exists( 'UACF7_Metabox' ) ) {
 			} else {
 				$tf_meta_box_value = array();
 			}
-			$metabox_request = ( ! empty( $_POST[ $this->metabox_id ] ) ) ? map_deep( wp_unslash( $_POST[ $this->metabox_id ] ),  'sanitize_text_field' ) : array();
+			$metabox_request = ( ! empty( $_POST[ $this->metabox_id ] ) ) ? map_deep( wp_unslash( $_POST[ $this->metabox_id ] ),  'wp_kses_post' ) : array();
 
 			if ( ! empty( $metabox_request ) && ! empty( $this->metabox_sections ) ) {
 				// uacf7_print_r($metabox_request);
